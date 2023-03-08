7997bd298c0ff75a2a3542fa3513d48f5193bc53
gangemi <a.gangemi@westpole.it>
2023-03-08 21:50:25 +0100

    first commit

---
+++ src/main/java/it/uniroma3/siw/model/Movie.java
@@ -0,0 +1,48 @@
+package it.uniroma3.siw.model;
+
+import javax.persistence.*;
+
+
+@Entity
+public class Movie {
+
+	@Id
+	@GeneratedValue (strategy = GenerationType.AUTO)
+	private Long id;
+    private String title;
+    private Integer year;
+    private String urlImage;
+    
+    public Movie() {
+    	super();
+    }
+    
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public Integer getYear() {
+		return year;
+	}
+	public void setYear(Integer year) {
+		this.year = year;
+	}
+	public String getUrlImage() {
+		return urlImage;
+	}
+	public void setUrlImage(String urlImage) {
+		this.urlImage = urlImage;
+	}
+    
+    
+    
+
+}
